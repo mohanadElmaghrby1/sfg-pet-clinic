9994b9b4a1239adf84f042d1b941b486d37c1905
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-05-30 20:57:05 +0200

    added PetType service

---
+++ pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package mohannad.springframework.sfgpetclinic.services;
+
+import mohannad.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType , Long> {
+}
