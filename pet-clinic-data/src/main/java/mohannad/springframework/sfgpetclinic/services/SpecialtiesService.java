bea42bdf0cd670ab444197002a3d09966501af58
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-05-30 22:07:01 +0200

    added specialities map imp

---
+++ pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package mohannad.springframework.sfgpetclinic.services;
+
+import mohannad.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality , Long> {
+}
