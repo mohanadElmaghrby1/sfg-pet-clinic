6947dc1eca38fac04b84609320052afd24ad36a8
mohannad <mohanad2021996@gmail.com>
2019-07-29 14:01:45 +0200

    add Visit Entity

--- pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Pet.java
@@ -1,10 +1,11 @@
 package mohannad.springframework.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -16,9 +17,15 @@ public class Pet  extends BaseEntity{
     @JoinColumn(name = "type_id")
     private PetType petType;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL , mappedBy = "pet")
+    private Set<Visit>visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +57,13 @@ public class Pet  extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Visit.java
@@ -1,10 +1,20 @@
 package mohannad.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
 
+@Entity
 public class Visit extends BaseEntity {
+
     private LocalDate date;
+
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
