0de656ef9f33663c97a4bb4fdda866e3cbf71294
mohannad <mohanad2021996@gmail.com>
2019-07-29 13:52:29 +0200

    update Vet to use JPA Entity

--- pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Speciality.java
@@ -1,5 +1,10 @@
 package mohannad.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     private String description ;
