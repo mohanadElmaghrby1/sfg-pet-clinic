5ce037b33f951a74176754c922b94e4737a01d5c
mohannad <mohanad2021996@gmail.com>
2019-07-29 13:41:16 +0200

    update entities with JPA Entity

--- pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/mohannad/springframework/sfgpetclinic/model/Pet.java
@@ -1,12 +1,23 @@
 package mohannad.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet  extends BaseEntity{
 
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
     private Owner owner;
+
     private LocalDate birthDate;
 
     public String getName() {
