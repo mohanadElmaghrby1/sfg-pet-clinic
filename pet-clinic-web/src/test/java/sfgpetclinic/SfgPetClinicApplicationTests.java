7a2b87ca535fa96d91affac6606c2ec6a3eb861f
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-08-02 21:17:55 +0200

    upgrade to Junit 5

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,12 +1,15 @@
 package sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.sun.glass.ui.Application;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class) //important its solve exeption 
 public class SfgPetClinicApplicationTests {
 
     @Test
